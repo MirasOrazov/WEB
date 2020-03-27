2b2c3dd5acc405402573466f20fe5c1ce54675b7
Orazov Miras <miras.orazov@gmail.com>
2020-03-28 00:20:04 +0600

    shop

---
+++ Shopback/api/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=300)
+    price = models.FloatField()
+    description = models.TextField(max_length=3000)
+    count = models.IntegerField()
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'description': self.description,
+            'count': self.count
+        }
---
+++ Shopback/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from api.views import product_list,product_detail,category_list,category_detail,product_category
+
+urlpatterns = [
+    path('products/', product_list),
+    path('products/<int:product_id>/', product_detail),
+    path('category/', category_list),
+    path('category/<int:category_id>/', category_detail),
+    path('category/<int:category_id>/products/', product_category),
+]
