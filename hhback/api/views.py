c0d49d19d22c9d552745488118f3dfa2b26a2eaa
Orazov Miras <miras.orazov@gmail.com>
2020-04-03 23:18:54 +0600

    hhback

---
+++ hhback/api/views.py
@@ -0,0 +1,52 @@
+from django.http.response import JsonResponse
+from django.http import Http404
+from django.db.models import Count
+
+from api.models import Company, Vacancy
+
+
+def company_list(request):
+    companies = Company.objects.all()
+    companies_json = [company.to_json() for company in companies]
+    return JsonResponse(companies_json, safe=False)
+
+
+def company_detail(request, company_id):
+    try:
+        company = Company.objects.get(id=company_id)
+    except Company.DoesNotExist as e:
+        return JsonResponse({'error': 'product does not exists'})
+
+    return JsonResponse(company.to_json())
+
+
+def company_vacancy(request, company_id):
+    try:
+        company = Company.objects.get(id=company_id)
+    except Company.DoesNotExist as e:
+        return JsonResponse({'error': str(e)})
+
+    vacancies = company.vacancy_set.all()
+    vacancies_json = [v.to_json() for v in vacancies]
+
+    return JsonResponse(vacancies_json, safe=False)
+
+
+def vacancy_list(request):
+    vacancies = Vacancy.objects.all()
+    vacancies_json = [vacancy.to_json() for vacancy in vacancies]
+    return JsonResponse(vacancies_json, safe=False)
+
+
+def vacancy_detail(request, vacancy_id):
+    try:
+        vacancy = Vacancy.objects.get(id=vacancy_id)
+        return JsonResponse(vacancy.to_json())
+    except Vacancy.DoesNotExist as e:
+        return JsonResponse({'error': str(e)})
+
+
+def top_vacancies(request):
+    vacancies = Vacancy.objects.annotate(Count('salary')).order_by('-salary')[:10]
+    vacancies_json = [vacancy.to_json() for vacancy in vacancies]
+    return JsonResponse(vacancies_json, safe=False)
